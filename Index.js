ca4c216a4824e5bcdadf130066b1d487d820fd94
sanket <sanketvghogare@gmail.com>
2025-05-31 00:12:13 +0530

    added default mongodb ip

--- Index.js
+++ Index.js
@@ -1,20 +1,24 @@
-import express, { json } from "express";
-import { connect } from "mongoose";
+import express from "express";
+import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 import feedbackRoutes from "./routes/feedbackRoutes.js";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(json());
+app.use(express.json());
 
-connect('mongodb+srv://sanketghogare75:LxEQbfouMGGYs4qu@cluster0.hg3kyfo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true 
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error(err));
+  .catch(err => console.error("MongoDB connection error:", err));
 
 app.use("/feedback", feedbackRoutes);
+
+app.get("/", (req, res) => {
+  res.send({ activeStatus: true, error: false });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
